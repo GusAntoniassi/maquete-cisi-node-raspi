8c0e11e035dce98759e54d93586f89a9bd24726e
GusAntoniassi <gus.antoniassi@gmail.com>
2018-08-14 13:01:42 -0300

    Atualização do fonte para criar o socket

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-let i2c = require('i2c');
-let arduino = new i2c(0x18, {device: '/dev/i2c-1', debug:false});
-arduino.setAddress(0x8);
-arduino.writeByte(0x2, function(err) { console.error(err); });
+/**
+ * @TODO:
+ * - Instanciar um socket server p/ escutar pelas mensagens
+ * - Tratar as mensagens por ponto e vírgula
+ * - Definir um padrão p/ mensagens
+ * - Verificar questão de ler os sensores do Arduino
+ */
+ 
+console.log('Inicializando maquete');
+ 
+/**
+ * Inicialização e configuração do socket
+ */
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
+http.listen(3000, () => {
+	console.log('Escutando na porta 3000');
+});
+
+/**
+ * Inicialização e configuração do Arduino por I2C
+ */
+//let i2c = require('i2c');
+//let arduino = new i2c(0x18, {device: '/dev/i2c-1', debug:false});
+//arduino.setAddress(0x8);
+//arduino.writeByte(0x2, (err) => { console.error(err); });
+
+io.on('connection', (socket) => {
+	console.log('Client conectado');
+	socket.on('disconnect', () => {
+		console.log('Client desconectado');
+	});
+	
+	/**
+	 * @TODO:
+	 * - Fazer o parse da mensagem recebida aqui 
+	 * - Enviar o respectivo comando para o Arduino
+	 */
+	
+	// Enviar mensagem para todos os clients
+	socket.emit('Client conectado');
+});
